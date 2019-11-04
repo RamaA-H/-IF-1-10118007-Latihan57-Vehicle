4ecc9c750e58d973b747c98e97f8be93fc5d97f5
Rama Al Halik <55701022+RamaA-H@users.noreply.github.com>
2019-11-04 16:32:13 +0700

    Add files via upload

---
+++ IF-1-10118007-Latihan57-Vehicle/src/latihan57/vehicle/Bicycle.java
@@ -0,0 +1,19 @@
+package latihan57.vehicle;
+public class Bicycle extends Vehicle{
+    
+    private int myGearCount;
+    
+    public Bicycle (){
+        System.out.println("Bicycle");
+    }
+
+    public int getMyGearCount() {
+        return myGearCount;
+    }
+
+    public void setMyGearCount(int myGearCount) {
+        this.myGearCount = myGearCount;
+    }
+    
+    
+}
---
+++ IF-1-10118007-Latihan57-Vehicle/src/latihan57/vehicle/Skateboard.java
@@ -0,0 +1,19 @@
+package latihan57.vehicle;
+public class Skateboard extends Vehicle{
+    
+    private double myBoardLength;
+    
+    public Skateboard (){
+        System.out.println("Skateboard");
+    }
+
+    public double getMyBoardLength() {
+        return myBoardLength;
+    }
+
+    public void setMyBoardLength(double myBoardLength) {
+        this.myBoardLength = myBoardLength;
+    }
+    
+    
+}
